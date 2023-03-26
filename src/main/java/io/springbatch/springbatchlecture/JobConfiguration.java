b1a9dc892e725fc2f5b0e39f84057f4c1cde731f
Jerry(이재헌) <ti6419@gmail.com>
2023-03-26 16:58:36 +0900

    feat: JobConfiguration 추가

---
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -0,0 +1,55 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job HelloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep1())
+                .next(helloStep2())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1() {
+        return stepBuilderFactory.get("helloStep")
+                .tasklet(((stepContribution, chunkContext) -> {
+
+                    System.out.println("========================");
+                    System.out.println(" >> Hello Spring Batch!!");
+                    System.out.println("========================");
+
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    public Step helloStep2() {
+        return stepBuilderFactory.get("helloStep")
+                .tasklet(((stepContribution, chunkContext) -> {
+
+                    System.out.println("========================");
+                    System.out.println(" >> Hello Violet Beach!!");
+                    System.out.println("========================");
+
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+}
