1d8be788ec65bf9ec779f8159f1f7e32c4a01724
Jerry(이재헌) <ti6419@gmail.com>
2023-05-06 22:30:36 +0900

    fix: Job 중복 실행 방지 로직 추가

--- src/main/java/io/springbatch/springbatchlecture/scheduler/FileScheduleJob.java
+++ src/main/java/io/springbatch/springbatchlecture/scheduler/FileScheduleJob.java
@@ -3,14 +3,16 @@ package io.springbatch.springbatchlecture.scheduler;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.quartz.JobExecutionContext;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -19,14 +21,30 @@ public class FileScheduleJob extends QuartzJobBean {
     private final Job fileJob;
     private final JobLauncher jobLauncher;
 
+    private final JobExplorer jobExplorer;
+
     @SneakyThrows
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) {
         String requestDate = jobExecutionContext.getJobDetail().getJobDataMap().get("requestDate").toString();
+
+        int jobInstanceCount = jobExplorer.getJobInstanceCount(fileJob.getName());
+        List<JobInstance> jobInstances = jobExplorer.getJobInstances(fileJob.getName(), 0, jobInstanceCount);
+        for (JobInstance jobInstance : jobInstances) {
+            List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstance);
+            boolean match = jobExecutions
+                    .stream()
+                    .anyMatch(jobExecution -> Objects.equals(jobExecution.getJobParameters().getString("requestDate"), requestDate));
+
+            if(match)
+                throw new JobExecutionException(requestDate + " already exists");
+        }
+
         JobParameters jobParams = new JobParametersBuilder()
                 .addLong("id", new Date().getTime())
                 .addString("requestDate", requestDate)
                 .toJobParameters();
+
         jobLauncher.run(fileJob, jobParams);
     }
 }
